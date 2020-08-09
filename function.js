c6c6f4077fe27b94197d0a62c011795587266fbc
bagasbawazir <bawazirfadhil@gmail.com>
2020-08-09 10:07:09 +0700

    fungtion 6

--- function.js
+++ function.js
@@ -67,3 +67,25 @@ exports.delete = function(req, res) {
             response.ok(rows, "Success delete diagnosa at id : " + id, res)
         }
     });
+
+  exports.login = function(req, res) {
+    
+    var username = req.query.username;
+    var password = req.query.password;
+
+    connection.query('SELECT * FROM user where username = ? and password = ?',
+    [ username,password ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            if(rows.length>0) {
+                response.ok(rows, "Success Login", res)
+            }
+            else {
+                response.failure(rows, "Login Failed", res)
+            }
+            
+        }
+    });
+};
