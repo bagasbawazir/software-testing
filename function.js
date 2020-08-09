1d8308313d85982a028acd642746a53c63513ea8
bagasbawazir <bawazirfadhil@gmail.com>
2020-08-09 10:05:58 +0700

    fungtion 3

--- function.js
+++ function.js
@@ -21,3 +21,17 @@ exports.getAllQuestion = function(req, res) {
         }
     });
 };
+
+exports.getById = function(req, res) {
+    
+    var username = req.params.username
+    connection.query('SELECT * FROM diagnosa WHERE username = ?',
+    [ username],
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, "Success Get Diagnosa", res)
+        }
+    });
+};
